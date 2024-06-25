32fdb4c121b07d23c5b05e574efead9727d5f9c5
Lucas de Paula Freitas <166523130+ofreitassx@users.noreply.github.com>
2024-06-25 10:16:36 -0300

    Add files via upload

---
+++ vaga de emprego.py
@@ -0,0 +1,11 @@
+print ("BEM VINDO A VAGA DE EMPREGO")
+idade = float(input("DIGIA A SUA IDADE: "))
+nacionalidade = str(input("DIGITE A SUA NACIONALIDADE: "))                # digitar o lugar que voce nasceu
+experiencia = float(input("DIGITE QUANTOS ANOS DE EXPERIENCIA VOCE TEM: "))    # digitar os seus anos de experiencia na area
+if idade >= 18:
+    if nacionalidade == "brasileiro" or nacionalidade == "Brasileira" :             # se a nacionalidade nao for brasileira ou brasileiro voce nao esta aprovado para a vaga de emprego
+        if experiencia >= 2:              # se anos de experiencia for menor que 2 voce nao esta aprovado para a vaga de trabalho
+            print ("VOCE FOI APROVADO PARA A VAGA DE EMPREGO")
+
+else:
+    print ("/////           VOCE NAO TEM TODOS OS REQUISITOS PARA PASSAR NA VAGA DE EMPREGO           /////")
