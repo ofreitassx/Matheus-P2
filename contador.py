eee40b322eca958cb35152e7c53c2d3ded9681a4
Lucas de Paula Freitas <166523130+ofreitassx@users.noreply.github.com>
2024-06-28 09:01:55 -0300

    Delete contador.py

--- contador.py
+++
@@ -1,4 +0,0 @@
-contador = 0
-while contador < 100:                      # enquanto o contador for menor que 100 ele adiciona 1 numero no contador
-    contador += 1                          # cada "+= 1" é um numero sendo adicionado no contador
-    print (f'contador: {contador}')        
